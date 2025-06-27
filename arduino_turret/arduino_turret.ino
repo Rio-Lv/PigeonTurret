e3c8f5eafeff3fd0258476fac399c0c98928667a
riolv <wong.rogerio@gmail.com>
2025-06-27 19:34:23 +0100

    further simplification

--- arduino_turret/arduino_turret.ino
+++ arduino_turret/arduino_turret.ino
@@ -1,24 +1,15 @@
-const int motorStepPin = 2;
-const int motorDirPin = 5;
-
-const int stepDelayMicroseconds = 500; // Adjust for speed: smaller = faster
-const long stepsToSpin = 5000; // Adjust for duration: more steps = longer spin
-
+#include <Stepper.h> 
+#define STEPS 200
+// Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver
+Stepper stepper(STEPS, 2, 3); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
+#define motorInterfaceType 1
+int Pval = 0;
+int potVal = 0;
 void setup() {
-  pinMode(motorStepPin, OUTPUT);
-  pinMode(motorDirPin, OUTPUT);
-
-  // Spin clockwise for a set number of steps
-  digitalWrite(motorDirPin, HIGH); // Set direction
-  for (long i = 0; i < stepsToSpin; i++) {
-    digitalWrite(motorStepPin, HIGH);
-    delayMicroseconds(stepDelayMicroseconds);
-    digitalWrite(motorStepPin, LOW);
-    delayMicroseconds(stepDelayMicroseconds);
-  }
-  // Motor stops after this loop completes
+  // Set the maximum speed in steps per second:
+  stepper.setSpeed(1000);
 }
-
 void loop() {
-  // Empty loop, motor stops after setup completes its spin
+  stepper.step(10);
+  delay(300);
 }
