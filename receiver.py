a7006982ef687a55dcf1c0c087391ba620e38201
riolv <wong.rogerio@gmail.com>
2025-07-13 16:14:28 +0100

    receiver to python 3.4 syntax

--- receiver.py
+++ receiver.py
@@ -1,24 +1,57 @@
+# receiver.py - Python 3.4 compatible TCP server
 import socket
+import sys
 
 HOST = "0.0.0.0"  # Listen on all interfaces
 PORT = 4444
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Prevent address reuse
+# Create socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+try:
     s.bind((HOST, PORT))
-    s.listen()
-    print(f"[*] Listening on {HOST}:{PORT}")
+    s.listen(5)
+    print("[*] Listening on {0}:{1}".format(HOST, PORT))
     
-    conn, addr = s.accept()
-    print(f"[+] Connected by {addr}")
+    while True:
+        try:
+            conn, addr = s.accept()
+            print("[+] New connection from {0}:{1}".format(addr[0], addr[1]))
+            
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    print("[-] Connection closed by client")
+                    break
+                command = data.decode('utf-8')
+                print("Received command: {0}".format(command))
+                
+        except socket.error as e:
+            # Handle connection reset errors specifically
+            if e.errno == 104:  # Connection reset by peer
+                print("[-] Connection reset by peer")
+            else:
+                print("Socket error: {0}".format(e))
+                
+        except KeyboardInterrupt:
+            print("\nServer interrupted")
+            break
+            
+        finally:
+            try:
+                conn.close()
+                print("[-] Connection closed")
+            except:
+                pass  # Ignore errors if conn is not defined
+                
+except socket.error as e:
+    print("Failed to start server: {0}".format(e))
+    sys.exit(1)
     
-    try:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"Received command: {data.decode()}")
-    except KeyboardInterrupt:
-        print("\nServer stopped")
-    finally:
-        conn.close()
+except KeyboardInterrupt:
+    print("\nServer stopped by user")
+
+finally:
+    s.close()
+    print("[*] Server shutdown complete")
