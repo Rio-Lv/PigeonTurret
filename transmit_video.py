231f5134b839e23a279e6404b05200ff7bd00c94
riolv <wong.rogerio@gmail.com>
2025-07-13 18:15:23 +0100

    update transmit video to 3.4 py

--- transmit_video.py
+++ transmit_video.py
@@ -1,4 +1,13 @@
-import subprocess 
+import subprocess
 
-COMMAND = "raspivid -w 640 -h 480 -vf -q 85 -t 0 -tl 100 -o - | nc -l -k 0 -p 3333"
-subprocess.run(COMMAND, shell=True)
+COMMAND = (
+    "raspivid -w 640 -h 480 -vf -q 85 -t 0 -tl 100 -o - "
+    "| nc -l -k 0 -p 3333"
+)
+
+# Blocks until the shell command exits
+subprocess.call(COMMAND, shell=True)
+
+# If you prefer to raise an exception when the command returns a
+# non-zero exit code, swap for:
+# subprocess.check_call(COMMAND, shell=True)
