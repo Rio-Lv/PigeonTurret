8d0b18b5b4b287f72da69b03ff117527373c1500
riolv <wong.rogerio@gmail.com>
2025-07-26 10:58:50 +0100

    update transmite video to how it was

---
+++ transmit_video.py
@@ -0,0 +1,13 @@
+import subprocess
+
+print("[*] Starting video transmission...")
+COMMAND = (
+    "raspivid -w 256 -h 256 -vf -fps 24  -t 0 -o - | nc -l -k -p 3333"
+)  # Start the camera streaming command
+
+# Blocks until the shell command exits
+subprocess.call(COMMAND, shell=True)
+
+# If you prefer to raise an exception when the command returns a
+# non-zero exit code, swap for:
+# subprocess.check_call(COMMAND, shell=True)
