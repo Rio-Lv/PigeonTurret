fce3b0a3347c4fd8bc1eaf7114b1a32cf479786c
riolv <wong.rogerio@gmail.com>
2025-08-03 15:32:56 +0100

    vertical flip back

--- transmit_video.py
+++ transmit_video.py
@@ -2,7 +2,7 @@ import subprocess
 
 # This command is optimized for low latency on older systems
 COMMAND = (
-    "stdbuf -o0 raspivid -w 512 -h 512 -fps 30 -ih -t 0 -o - "
+    "stdbuf -o0 raspivid -w -vf 512 -h 512 -fps 30 -ih -t 0 -o - "
     "| nc -u -l -p 3333"
 )
 
