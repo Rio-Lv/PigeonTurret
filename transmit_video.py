a582ed0b5068d6ee3953cdf7c29dc3bf9bd1766d
riolv <wong.rogerio@gmail.com>
2025-08-03 15:59:37 +0100

    removed stdbugg

--- transmit_video.py
+++ transmit_video.py
@@ -2,7 +2,7 @@ import subprocess
 
 # This command is optimized for low latency on older systems
 COMMAND = (
-    "stdbuf -o0 raspivid -w 512 -h 512 -vf -fps 30 -ih -t 0 -o - "
+    "raspivid -w 512 -h 512 -vf -fps 30 -ih -t 0 -o - "
     "| nc -u 192.168.1.120 3333"
 )
 
